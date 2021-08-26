9eb3315558af9dd216d3bf2ff07e2001785a08c1
Song0129 <Song_3191@163.com>
2021-08-26 10:47:54 +0800

    update

--- redux_test/README.md
+++ redux_test/README.md
@@ -36,3 +36,18 @@
         2).创建action的函数不再返回一般对象，而是一个函数，该函数中写异步任务
         3).异步任务有结果后，分发一个同步action去真正操作数据
     (4).备注：异步action不是必须要写的，完全可以自己等待异步任务的结果有了再去分发同步action
+
+## 4.求和案例\_react-redux 优化
+
+    (1).容器组件和UI组件整个为一个文件
+    (2).无需自己给容器组件传递store，给<App/>包裹一个<Provider store={store}>即可
+    (3).使用了react-redux后也不用再自己检测redux中状态的改变了，容器组件可以自动完成整个工作
+    (4).mapDispatchToProps也可以简单的写成一个对象
+    (5).一个组件要和redux“打交道”要经过哪几步？
+        1).定义好UI组件---不暴露
+        2).引入connect生成一个容器组件，并暴露，写法如下：
+            connect(
+                state=>({key:value}),//映射状态
+                {key:xxxAction}//映射操作状态的方法
+            )(UI组件)
+        3).在UI组件中通过this.props.xxx  读取和操作状态
--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -1,9 +1,80 @@
-// 引入Count的UI组件
-import CountUI from "../../components/Count";
+import React, { Component } from "react";
+import { Button, Select } from "antd";
+
 // 引入action
 import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from "../../redux/count_action";
 // 引入connect用于连接UI组件和redux
 import { connect } from "react-redux";
+
+const { Option } = Select;
+
+class Count extends Component {
+	state = {
+		value: 1,
+	};
+
+	// 下拉框回调
+	handleChange = (value, Option) => {
+		this.setState({ value });
+	};
+
+	// 加法
+	increment = () => {
+		const { value } = this.state;
+		this.props.jia(value * 1);
+	};
+
+	// 减法
+	dencrement = () => {
+		const { value } = this.state;
+		this.props.jian(value * 1);
+	};
+
+	// 奇数再加
+	incrementIfOdd = () => {
+		const { value } = this.state;
+		if (this.props.count % 2 !== 0) {
+			this.props.jia(value);
+		}
+	};
+
+	// 异步加
+	incrementAsync = () => {
+		const { value } = this.state;
+		this.props.jiaAsync(value, 500);
+	};
+
+	render() {
+		console.log(this.props);
+		const { value } = this.state;
+		return (
+			<div>
+				<h2>当前求和为：{this.props.count}</h2>
+				<Select defaultValue={value} style={{ width: 120 }} onChange={this.handleChange}>
+					<Option value="1">1</Option>
+					<Option value="2">2</Option>
+					<Option value="3">3</Option>
+				</Select>
+				&nbsp;
+				<Button onClick={this.increment} type="primary">
+					+
+				</Button>
+				&nbsp;
+				<Button onClick={this.dencrement} type="primary">
+					-
+				</Button>
+				&nbsp;
+				<Button onClick={this.incrementIfOdd} type="primary">
+					和为奇数+
+				</Button>
+				&nbsp;
+				<Button onClick={this.incrementAsync} type="primary">
+					异步+
+				</Button>
+			</div>
+		);
+	}
+}
 
 // 使用connect()()创建并暴露一个Count的容器组件
 export default connect(
@@ -25,4 +96,4 @@ export default connect(
 		jian: createDecrementAction,
 		jiaAsync: createIncrementAsyncAction,
 	}
-)(CountUI);
+)(Count);

a00b4cdae5c93e635b23ded7e4cd0fc8c5108612
Song0129 <Song_3191@163.com>
2021-08-27 14:57:04 +0800

    update

---
+++ react_extension/src/components/7_renderProps/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./index.css";
+
+export default class Parent extends Component {
+	render() {
+		return (
+			<div className="parent">
+				<h2>我是Parent组件</h2>
+				<A render={(state) => <B state={state} />} />
+			</div>
+		);
+	}
+}
+
+class A extends Component {
+	state = {
+		name: "tom",
+	};
+	render() {
+		console.log(this.props);
+		const { name } = this.state;
+		return (
+			<div className="a">
+				<h2>我是A组件</h2>
+				{this.props.render(this.state)}
+			</div>
+		);
+	}
+}
+
+class B extends Component {
+	render() {
+		console.log("b-----render", this.props);
+		return (
+			<div className="b">
+				<h2>我是B组件</h2>
+				<span>这是Parent组件传过来的名字:{this.props.state.name}</span>
+			</div>
+		);
+	}
+}
